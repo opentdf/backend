278afc57a4f7dba2bd78d6efc56ba8b217d013ca
Ben Leggett <bleggett@virtru.com>
2021-12-14 13:11:21 -0500

    Add basic keycloak forced auth

--- hackathon_2021_2/src/index.js
+++ hackathon_2021_2/src/index.js
@@ -5,6 +5,37 @@ import "regenerator-runtime/runtime";
 import React from "react";
 import ReactDOM from "react-dom";
 
+//OIDC shenanigans
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import Keycloak from "keycloak-js";
+
 import ABACShip from './abacship';
 
-ReactDOM.render(<ABACShip />, document.getElementById("react-root"));
+//Obviously these would not be hardcoded normally
+const KEYCLOAK_HOST = "http://localhost:65432/keycloak/auth/"
+const KEYCLOAK_CLIENT_ID = "browsertest"
+const KEYCLOAK_REALM = "tdf"
+
+const keycloak = new Keycloak({
+  url: KEYCLOAK_HOST,
+  clientId: KEYCLOAK_CLIENT_ID,
+  realm: KEYCLOAK_REALM,
+});
+
+ReactDOM.render(
+    <ReactKeycloakProvider
+      authClient={keycloak}
+      initOptions={{
+        onLoad: 'login-required',
+      }}
+      onEvent={(event, error) => {
+        console.log("onKeycloakEvent", event, error);
+      }}
+      onTokens={(tokens) => {
+        sessionStorage.setItem("keycloak", tokens.token || "");
+      }}
+    >
+        <ABACShip />
+    </ReactKeycloakProvider>,
+    document.getElementById("react-root"),
+);
