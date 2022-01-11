823932932c45d684dd4f045826231901dd7f44a3
David Mihalcik <dmihalcik@virtru.com>
2022-01-11 13:41:12 -0500

    ⬆️✅ Updates packages; lets unit tests run in CI
    - Updates packages, notably to allow use of later pytests, needed for python 3.10
    - Fixes some tests
    - Fixes login to github contianer registry
    - Fix tilt's inference of which python-base image to use
    
    review notes

--- containers/attributes/tests/test_main.py
+++ containers/attributes/tests/test_main.py
@@ -1,10 +1,10 @@
-from ..main import app
+from attributes.main import app
 from fastapi.testclient import TestClient
 
-client = TestClient(app)
 
+client = TestClient(app)
 
 def test_read_semver():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"Hello": "attributes"}
