b11bd39d924f2e42fde0f836e685b5e371f884ae
David Mihalcik <dmihalcik@virtru.com>
2022-03-22 11:29:54 -0400

    fix loading version files

--- containers/entitlements/__init__.py
+++ containers/entitlements/__init__.py
@@ -1,2 +1,6 @@
-with open("VERSION", "r") as ver:
+import os
+
+curr_dir = os.path.dirname(__file__)
+path = os.path.join(curr_dir, "VERSION")
+with open(path, "r") as ver:
     __version__ = ver.read().rstrip()
