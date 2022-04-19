935428454819345b3f5db3c15d0a684b08087611
Yustinian Sizariev <ysizariev@lohika.com>
2022-04-19 18:01:43 +0300

    fix: helm charts pipeline (#1706)

--- .github/scripts/push_helm-charts.sh
+++
@@ -1,8 +0,0 @@
-for d in */ ; do
-  if [ -f "$d/Chart.yaml" ]; then
-    echo "Rendering Helm chart $d to validate defaults..."
-    helm package $d
-    helm push *.tgz oci://ghcr.io/opentdf/charts
-    rm *.tgz
-  fi
-done
