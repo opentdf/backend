38003cc0f5f7c4cdfb7f5fe97b140f0de52fd325
Ben Leggett <854255+bleggett@users.noreply.github.com>
2022-04-19 10:30:32 -0400

    Update containers/claims/tests/conftest.py
    Co-authored-by: Dave Mihalcik <38867245+dmihalcik-virtru@users.noreply.github.com>

--- containers/claims/tests/conftest.py
+++ containers/claims/tests/conftest.py
@@ -4,6 +4,6 @@ from fastapi.testclient import TestClient
 from ..main import app
 
 @pytest.fixture(scope="module")
-def test_app():
+def client_fixture():
     client = TestClient(app)
     yield client  # testing happens here
