f47fc92c3c87b9f25a1a502b432bca2afc26b602
Paul Flynn <pflynn@virtru.com>
2022-05-12 12:08:53 -0400

    attrName test

--- tests/containers/clients/py/service.py
+++ tests/containers/clients/py/service.py
@@ -17,37 +17,24 @@ def main():
     scope = 'openid email profile'
     client = OAuth2Session(auth[1], auth[2], scope=scope)
 
-    authorization_endpoint = args.oidcEndpoint + '/auth/realms/' + auth[0] + '/protocol/openid-connect/auth'
-    print(authorization_endpoint)
-    uri, state = client.create_authorization_url(authorization_endpoint, response_type='token')
-    print(uri)
-
     token_endpoint = args.oidcEndpoint + '/auth/realms/' + auth[0] + '/protocol/openid-connect/token'
-    print(token_endpoint)
     token = client.fetch_token(token_endpoint, grant_type='client_credentials')
-    print(token)
     auth = OAuth2Auth(token)
     response = requests.get(args.attributesEndpoint, auth=auth)
-    print(response.text)
     assert response.ok
     # Authorities
     authorities_endpoint = f'{args.attributesEndpoint}/authorities'
-    print(authorities_endpoint)
     response = requests.get(authorities_endpoint, auth=auth)
-    print(response.text)
     assert response.ok
     # Authorities Create
     authority = {
         "authority": "https://opentdf.io"
     }
     response = requests.post(authorities_endpoint, json=authority, auth=auth)
-    print(response.status_code)
     assert response.ok or response.status_code == 400
     # Attributes Definitions
     attributes_definitions_endpoint = f'{args.attributesEndpoint}/definitions/attributes'
-    print(attributes_definitions_endpoint)
     response = requests.get(attributes_definitions_endpoint, auth=auth)
-    print(response.text)
     assert response.ok
     # Attributes Definitions Create
     attribute_ip = {
@@ -63,7 +50,6 @@ def main():
         ]
     }
     response = requests.post(attributes_definitions_endpoint, json=attribute_ip, auth=auth)
-    print(response.status_code)
     assert response.ok or response.status_code == 400
     attribute_sg = {
         "authority": "https://opentdf.io",
@@ -79,7 +65,6 @@ def main():
         ]
     }
     response = requests.post(attributes_definitions_endpoint, json=attribute_sg, auth=auth)
-    print(response.status_code)
     assert response.ok or response.status_code == 400
     # Attributes Definitions Update
     attribute_sg = {
@@ -97,19 +82,22 @@ def main():
         ]
     }
     response = requests.put(attributes_definitions_endpoint, json=attribute_sg, auth=auth)
-    print(response.status_code)
     assert response.ok
     # Attributes Definitions Get
     response = requests.get(attributes_definitions_endpoint,
                             params={"authority": "https://opentdf.io","name": "SocialGrade"}, auth=auth)
-    print(response.text)
     assert response.ok
     assert "C1" in response.text
     response = requests.get(attributes_definitions_endpoint,
                             params={"authority": "https://opentdf.io","name": "IntellectualProperty"}, auth=auth)
-    print(response.text)
     assert response.ok
     assert "C1" not in response.text
+    # Attribute rule endpoint for KAS
+    attributes_names_endpoint = f'{args.attributesEndpoint}/v1/attrName'
+    response = requests.post(attributes_names_endpoint)
+    assert response.ok
+    assert "SocialGrade" in response.text
+    assert "IntellectualProperty" in response.text
 
 
 if __name__ == "__main__":
