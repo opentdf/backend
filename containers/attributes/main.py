4d5c077ce19931b523479b4cd4b0c853f7297edf
Ben Leggett <bleggett@virtru.com>
2022-06-23 17:59:32 -0400

    Derp

--- containers/attributes/main.py
+++ containers/attributes/main.py
@@ -257,14 +257,6 @@ async def add_response_headers(request: Request, call_next):
     response.headers["X-Content-Type-Options"] = "nosniff"
     return response
 
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = time.time() - start_time
-    response.headers["X-Process-Time"] = str(process_time)
-    return response
-
 # OpenAPI
 tags_metadata = [
     {
