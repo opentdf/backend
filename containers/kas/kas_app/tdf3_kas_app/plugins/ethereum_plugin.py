b9fec1ee7e02301677e7a2c8fa97f8665b99cda2
Paul Flynn <pflynn@virtru.com>
2022-07-13 13:36:08 -0400

    data_attributes

--- containers/kas/kas_app/tdf3_kas_app/plugins/ethereum_plugin.py
+++ containers/kas/kas_app/tdf3_kas_app/plugins/ethereum_plugin.py
@@ -4,11 +4,12 @@ from tdf3_kas_core.abstractions import AbstractRewrapPlugin
 from tdf3_kas_core.authorized import looks_like_jwt
 from tdf3_kas_core.errors import UnauthorizedError
 
+from containers.kas.kas_core.tdf3_kas_core import Policy
+
 
 def get_entity_pubkey(access_jwt):
     # grab information out of the token without verifying it.
     decoded_jwt = jwt.decode(
-        # This could be an access_token or refresh_token
         access_jwt,
         options={"verify_signature": False, "verify_aud": False},
         algorithms=["RS256", "ES256", "ES384", "ES512"],
@@ -20,7 +21,7 @@ def get_entity_pubkey(access_jwt):
 class EthereumPlugin(AbstractRewrapPlugin):
     def update(self, req, res):
         print(req)
-        # Get bearer token
+        # entity_pubkey from bearer token
         try:
             auth_token = req.context.data["Authorization"]
             bearer, _, idp_jwt = auth_token.partition(" ")
@@ -33,4 +34,17 @@ class EthereumPlugin(AbstractRewrapPlugin):
         print("~~~~~~~~~")
         print(entity_pubkey)
         print("~~~~~~~~~")
+        # attributes from policy
+        data = req.json()
+        decoded_tdf_request = jwt.decode(
+            data["signedRequestToken"],
+            options={"verify_signature": False, "verify_aud": False},
+            algorithms=["RS256", "ES256", "ES384", "ES512"],
+        )
+        print(decoded_tdf_request)
+        canonical_policy = data["policy"]
+        original_policy = Policy.construct_from_raw_canonical(canonical_policy)
+        print("^^^^^^^^")
+        print(original_policy.data_attributes)
+        print("^^^^^^^^")
         return req, res
