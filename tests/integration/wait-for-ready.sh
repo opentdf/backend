9ea8d7e4b898ac2fc565fbfb89f485cdc51512b4
Ben Leggett <bleggett@virtru.com>
2022-07-25 19:17:40 -0400

    Fix this

--- tests/integration/wait-for-ready.sh
+++ tests/integration/wait-for-ready.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 [jobname] [duration] [namespace]"
+    exit
+fi
 # Since `tilt` isn't good at waiting for normal Kube resources it hasn't previously created itself/pretends things it didn't create don't exist, do that here.
 
 echo "**************************************"
-echo "Waiting (up to 10 min) for keycloak-bootstrap job to complete"
+echo "Waiting (up to $2) for $1 job to complete"
 echo "**************************************"
-if kubectl get job/keycloak-bootstrap -n default; then kubectl wait --for=condition=complete --timeout=10m job/keycloak-bootstrap; fi
+if kubectl get $1 -n $3; then kubectl wait --for=condition=complete --timeout=$2 $1; fi
