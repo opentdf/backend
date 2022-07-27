fb4a7e60b52edb24168f2e7eb6a40764f0cac6aa
Ben Leggett <bleggett@virtru.com>
2022-07-27 14:10:55 -0400

    Fix TINI

--- containers/kas/tini-init.sh
+++ containers/kas/tini-init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+/opentdf/bin/access-pdp-grpc-server &
 gunicorn --statsd-host "${STATSD_HOST}:${STATSD_PORT}" --limit-request-field_size 65535 --statsd-prefix "service.kas" \
     --config "gunicorn.conf.py" --bind ":8000" wsgi:app &
-/opentdf/bin/access-pdp-grpc-server &
 wait -n
