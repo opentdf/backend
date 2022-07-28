6bbd4914fd36ebd64ebddedbdc818d651c3c503e
Ben Leggett <bleggett@virtru.com>
2022-07-28 16:50:57 -0400

    Fix this

--- containers/kas/kas_app/tdf3_kas_app/kas_app.py
+++ containers/kas/kas_app/tdf3_kas_app/kas_app.py
@@ -8,7 +8,7 @@ from importlib.metadata import PackageNotFoundError
 
 from tdf3_kas_core import Kas
 
-from accesspdp_client_python.accesspdp.v1 import accesspdp_pb2
+from accesspdp.v1 import accesspdp_pb2
 
 from .plugins import opentdf_attr_authority_plugin, revocation_plugin
 
