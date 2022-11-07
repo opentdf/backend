606c5319331dda65977b704aaa9c723e73527c43
Elizabeth Healy <ehealy@virtru.com>
2022-11-07 15:28:27 -0500

    enabled audit logger

--- containers/entitlements/main.py
+++ containers/entitlements/main.py
@@ -36,7 +36,20 @@ from sqlalchemy import and_, or_
 from sqlalchemy.orm import Session, sessionmaker, declarative_base
 
 logging.basicConfig(stream=sys.stdout, level=os.getenv("SERVER_LOG_LEVEL", "CRITICAL"))
+AUDIT_ENABLED = os.getenv("AUDIT_ENABLED", "false").lower() in ("yes", "true", "t", "1")
+if AUDIT_ENABLED:
+    AUDIT_LEVEL_NUM = os.getenv("AUDIT_LEVEL_NUM", 45)
+    logging.addLevelName(AUDIT_LEVELV_NUM, "AUDIT")
+    def audit(self, message, *args, **kws):
+        if self.isEnabledFor(AUDIT_LEVELV_NUM):
+            # Yes, logger takes its '*args' as 'args'.
+            self._log(AUDIT_LEVELV_NUM, message, args, **kws) 
+    logging.Logger.audit = audit
 logger = logging.getLogger(__package__)
+
+if AUDIT_ENABLED:
+    logger.audit("DUMMY LOG AUDIT")
+
 
 swagger_ui_init_oauth = {
     "usePkceWithAuthorizationCodeGrant": True,
