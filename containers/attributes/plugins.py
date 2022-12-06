a0c4d8503270545a96b5c03d1ef9f03e0867b1de
Elizabeth Healy <35498075+elizabethhealy@users.noreply.github.com>
2022-12-06 12:02:17 -0500

    PLAT-2121 stub audit logger for attributes and entitlements (#335)
    * enabled audit logger
    
    * comment everything out
    
    * audit log level working in all 3
    
    * dont require if statement for audit log statements
    
    * dummy log create attribute
    
    * datetime
    
    * return type
    
    * replace with stubs
    
    * update plugins
    
    * try fix entitlements tests
    
    * try fixing attribute tests
    
    * trigger github actions
    
    * bump cryptography
    
    * fix sqlite vulnerability

---
+++ containers/attributes/plugins.py
@@ -0,0 +1,19 @@
+# STUB
+
+from enum import Enum
+
+class HttpMethod(Enum):
+    GET = 4
+    POST = 5
+    PUT = 6
+    PATCH = 7
+    DELETE = 8
+
+def run_pre_command_hooks(*args):
+    pass
+
+def run_post_command_hooks(*args):
+    pass
+
+def run_err_hooks(*args):
+    pass
