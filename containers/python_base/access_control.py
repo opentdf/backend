ae5928410c39861e438720213c461a8069d74875
Mikhail Ustyantsev <mustyantsev@lohika.com>
2022-12-13 17:06:23 +0200

    changes according to comments

--- containers/python_base/access_control.py
+++ containers/python_base/access_control.py
@@ -3,29 +3,11 @@ import os
 from keycloak import KeycloakOpenID
 
 logger = logging.getLogger(__package__)
+
+
 def add_filter_by_access_control(request):
-    authority = get_authority(request)
-    return authority
+    pass
+
 
 def get_authority(request):
-    token = request.headers.get('Authorization').replace('Bearer ', '')
-    keycloak_openid = KeycloakOpenID(
-        server_url=os.getenv("OIDC_SERVER_URL"),
-        client_id=os.getenv("OIDC_CLIENT_ID"),
-        realm_name=os.getenv("OIDC_REALM"),
-        client_secret_key=os.getenv("OIDC_CLIENT_SECRET"),
-        verify=True,
-    )
-
-    decoded = keycloak_openid.decode_token(
-        token,
-        key="",
-        options={"verify_signature": False, "verify_aud": False, "exp": True},
-    )
-
-    if "orgs_domain" in decoded:
-        authority = "https://" + decoded["orgs_domain"]
-    else:
-        authority = None
-
-    return authority
+    pass
