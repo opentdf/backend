355da67d1fd872e558144d6457f59f7ecb18a05b
Elizabeth Healy <ehealy@virtru.com>
2023-01-26 15:40:24 -0500

    rename hooks file

--- containers/kas/kas_app/tdf3_kas_app/kas_app.py
+++ containers/kas/kas_app/tdf3_kas_app/kas_app.py
@@ -12,7 +12,7 @@ from .plugins import (
     opentdf_attr_authority_plugin,
     revocation_plugin,
     access_pdp_healthz_plugin,
-    hooks
+    audit_hooks
 )
 
 logger = logging.getLogger(__name__)
@@ -103,8 +103,8 @@ def app(name):
     kas.use_healthz_plugin(access_pdp_health)
 
     if AUDIT_ENABLED:
-        kas.use_post_rewrap_hook(hooks.audit_hook)
-        kas.use_err_rewrap_hook(hooks.err_audit_hook)
+        kas.use_post_rewrap_hook(audit_hooks.audit_hook)
+        kas.use_err_rewrap_hook(audit_hooks.err_audit_hook)
 
     configure_filters(kas)
 
