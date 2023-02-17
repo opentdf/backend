8db5dabc3c3b9773918b905092cf12e12f043d3e
Paul Flynn <pflynn@virtru.com>
2023-02-17 13:43:11 -0500

    go

--- containers/configuration/main.go
+++ containers/configuration/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/go-redis/redis"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	fmt.Println("Go Redis Tutorial")
+	log.Println("Configuration service starting")
 
 	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
@@ -17,5 +22,33 @@ func main() {
 
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
+
+	// os interrupt
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	// server
+	server := http.Server{
+		Addr:         "127.0.0.1:8080",
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+	http.HandleFunc("/configuration", Handler)
+	go func() {
+		log.Printf("listening on http://%s", server.Addr)
+		log.Printf(os.Getenv("SERVICE"))
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	<-stop
+	err = server.Shutdown(context.Background())
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func Handler(writer http.ResponseWriter, request *http.Request) {
 
 }
