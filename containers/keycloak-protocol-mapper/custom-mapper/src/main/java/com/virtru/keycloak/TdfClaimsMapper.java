7be3dc14e38b0bcccee99b7cbf783bc929453ee8
Elizabeth Healy <ehealy@virtru.com>
2023-08-22 12:04:10 -0400

    remove extra logging

--- containers/keycloak-protocol-mapper/custom-mapper/src/main/java/com/virtru/keycloak/TdfClaimsMapper.java
+++ containers/keycloak-protocol-mapper/custom-mapper/src/main/java/com/virtru/keycloak/TdfClaimsMapper.java
@@ -151,8 +151,6 @@ public class TdfClaimsMapper extends AbstractOIDCProtocolMapper
         //
         // We will have to fix `dissems` to properly get rid of this hack.
         
-        // ObjectMapper objectMapper = new ObjectMapper();
-        // logger.info("Mapper token [{}]", objectMapper.writeValueAsString(token));
         token.setSubject(userSession.getUser().getId());
         logger.info("TDF claims mapper triggered");
 
