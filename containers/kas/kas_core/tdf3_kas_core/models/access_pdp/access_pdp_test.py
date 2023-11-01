7d632ff2936f98d683036c5808be41060646cb78
sivanov <sivanov.ctr@virtru.com>
2023-11-01 12:49:38 +0200

    not returning value None

--- containers/kas/kas_core/tdf3_kas_core/models/access_pdp/access_pdp_test.py
+++ containers/kas/kas_core/tdf3_kas_core/models/access_pdp/access_pdp_test.py
@@ -8,7 +8,7 @@ class TestAccessPDP(unittest.TestCase):
     def setUp(self):
         self.access_pdp = AccessPDP()
 
-    @patch('tdf3_kas_core.pdp_grpc.convert_attribute_defs', return_value=None)
+    @patch('tdf3_kas_core.pdp_grpc.convert_attribute_defs', side_effect=lambda x: None)
     def test_check_attributes_with_none_attr_defs(self, _):
         data_attributes = Mock()
         entity_attributes = Mock()
