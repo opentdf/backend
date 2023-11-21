9f296f28ba0123ba4350cc696ae5519c23b244e9
Elizabeth Healy <ehealy@virtru.com>
2023-11-21 10:08:35 -0500

    try using wsgi_app

--- containers/kas/kas_core/tdf3_kas_core/kas.py
+++ containers/kas/kas_core/tdf3_kas_core/kas.py
@@ -392,7 +392,8 @@ class Kas(object):
 
         logger.debug("KAS app starting.")
         # convert from asgi to wsgi
-        self._app = ASGIMiddleware(app)
+        #self._app = ASGIMiddleware(app)
+        self._app = app.wsgi_app
         return self._app
 
 
